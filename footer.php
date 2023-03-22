df9c33d9f098e49b9552a4e87f2c969ac307db88
Gabbie Bade <gabbiebade@gmail.com>
2023-03-21 19:13:17 -0700

    change socials into tempate part

--- footer.php
+++ footer.php
@@ -21,34 +21,21 @@
 				<?php endif;
 				if(get_field('contact_heading', 'option')): ?>
 					<h2><?php the_field('contact_heading', 'option'); ?></h2>
-				<?php endif; ?>
-				<section class="contact-info">
+					<?php endif; ?>
+					<section class="contact-info">
 				<?php if(have_rows('contact_details', 'option')): the_row();
 				
-					$group = get_field('contact_details', 'option');
-					?> <h3><?php the_sub_field('heading'); ?></h3><?php
-
-					// print_r($group['social_media']);
-
-					$socials = $group['social_media'];
-					if(have_rows('social_media', 'option')){
-						while(have_rows('social_media', 'option')): the_row(); 
-						$link = get_sub_field('social_media_link');
-
-						$link_url = $link['url'];
-						$link_title = $link['title'];
-						$link_target = $link['target'] ? $link['target'] : '_self';
-						?>
-						
-						<a href="<?php echo $link_url; ?>" target="<?php echo $link_target; ?>">
-						<figure>
-							<img src="<?php the_sub_field('social_media_icon'); ?>" alt="<?php echo $link_title;?>" class="social-icons">
-							<figcaption><?php echo $link_title;?></caption>
-						</figure>
-						</a>
-						<?php endwhile;
-					}
-					endif; ?>
+				$group = get_field('contact_details', 'option');
+				?> <h3><?php the_sub_field('heading'); ?></h3>
+					<ul>
+						<li><a href="mailto:<?php the_sub_field('company_email'); ?>"><?php the_sub_field('company_email'); ?></a></li>
+						<li><a href="tel:+1<?php the_sub_field('company_phone'); ?>">+1<?php the_sub_field('company_phone'); ?></a></li>
+					</ul>
+					<?php
+				endif; 
+				
+				get_template_part( 'template-parts/content', 'contact-socials');
+				?>
 					</div>
 					<div class="footer-cta-form">
 						<?php echo do_shortcode('[gravityform id="1" title="false"]') ?>
