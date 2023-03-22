c0839559680c2d565497c20d25fe8956c7c1dbc5
katy-chen <118566165+katy-chen@users.noreply.github.com>
2023-03-22 16:24:36 -0700

    Fixed spacing for best sellers in archive products page

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -49,33 +49,30 @@ function walkies_landing_page($page_id) {
     if( get_field( 'best_sellers_heading', $page_id ) && $best_sellers ):
       echo '<section class="walkies_best_sellers">';
       echo "<h2>" . get_field( 'best_sellers_heading', $page_id ) . "</h2>";
-      // best_sellers
-      // Relationship Fields 
 
-   
-		foreach( $best_sellers as $best_seller ) : ?>
-			<article class="bestseller-cards">
-				<?php
-				echo get_the_post_thumbnail($best_seller);
-				echo get_the_title($best_seller -> ID);
+      foreach( $best_sellers as $best_seller ) : ?>
+        <article class="bestseller-cards">
+          <?php
+          echo get_the_post_thumbnail($best_seller);
+          echo get_the_title($best_seller -> ID);
 
-				$post_categories = wp_get_post_categories($best_seller -> ID );
-				$cats = array();
-				
-				global $post;
-				$terms = get_the_terms( $best_seller->ID, 'product_cat');
-				foreach($terms as $term) :
-					if( $term->term_id != 27) :
-						echo '<p>'.esc_html($term->name).'<p>';
-					endif;
-				endforeach;	
-				
-				$product = wc_get_product( $best_seller->ID );
-				echo $product->get_price_html();
-				?>			
-			</article>
-			<?php			
-		endforeach;	
+          $post_categories = wp_get_post_categories($best_seller -> ID );
+          $cats = array();
+          
+          global $post;
+          $terms = get_the_terms( $best_seller->ID, 'product_cat');
+          foreach($terms as $term) :
+            if( $term->term_id != 27) :
+              echo '<p>'.esc_html($term->name).'<p>';
+            endif;
+          endforeach;	
+          
+          $product = wc_get_product( $best_seller->ID );
+          echo $product->get_price_html();
+          ?>			
+        </article>
+        <?php			
+      endforeach;	
 
       echo '</section>';
     endif;
