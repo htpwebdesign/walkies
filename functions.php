f8796034870e8fe3eacd2458f533405d5ead22b4
MiRim Yu <firepunch119@gmail.com>
2023-04-03 11:12:01 -0700

    remove category slug function

--- functions.php
+++ functions.php
@@ -257,24 +257,3 @@ add_action('acf/init', 'gfw_contact_page_acf');
 
 // Hide Archive Prefix
 add_filter( 'get_the_archive_title_prefix', '__return_empty_string' );
-
-// Remove product-category slug
-// Plugin URI: https://timersys.com/
-add_filter('request', function( $vars ) {
-	global $wpdb;
-	if( ! empty( $vars['pagename'] ) || ! empty( $vars['category_name'] ) || ! empty( $vars['name'] ) || ! empty( $vars['attachment'] ) ) {
-		$slug = ! empty( $vars['pagename'] ) ? $vars['pagename'] : ( ! empty( $vars['name'] ) ? $vars['name'] : ( !empty( $vars['category_name'] ) ? $vars['category_name'] : $vars['attachment'] ) );
-		$exists = $wpdb->get_var( $wpdb->prepare( "SELECT t.term_id FROM $wpdb->terms t LEFT JOIN $wpdb->term_taxonomy tt ON tt.term_id = t.term_id WHERE tt.taxonomy = 'product_cat' AND t.slug = %s" ,array( $slug )));
-		if( $exists ){
-			$old_vars = $vars;
-			$vars = array('product_cat' => $slug );
-			if ( !empty( $old_vars['paged'] ) || !empty( $old_vars['page'] ) )
-				$vars['paged'] = ! empty( $old_vars['paged'] ) ? $old_vars['paged'] : $old_vars['page'];
-			if ( !empty( $old_vars['orderby'] ) )
-	 	        	$vars['orderby'] = $old_vars['orderby'];
-      			if ( !empty( $old_vars['order'] ) )
- 			        $vars['order'] = $old_vars['order'];	
-		}
-	}
-	return $vars;
-});
