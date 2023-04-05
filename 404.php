d5c856608b18164c0cfe09d43613aac82609d921
Gabbie Bade <gabbiebade@gmail.com>
2023-04-05 12:53:50 -0700

    404 styled and php completed

--- 404.php
+++ 404.php
@@ -13,12 +13,13 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<section class="error-404 not-found">
+			<img src="https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/256/external-dog-space-vitaliy-gorbachev-flat-vitaly-gorbachev.png" alt="Dog graphic by Vitaliy Gorbachev from icons8.com">
 			<header class="page-header">
 				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'walkies' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'walkies' ); ?></p>
+				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links above or a search?', 'walkies' ); ?></p>
 
 					<?php
 					get_search_form();
