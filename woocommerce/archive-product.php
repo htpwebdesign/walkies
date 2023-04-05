b252efba410515f55b370b3b087cb1fd20aafcf9
MiRim Yu <firepunch119@gmail.com>
2023-04-05 12:31:07 -0700

    styling products page

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -26,5 +26,8 @@ if ( is_shop() )
   walkies_list_page( 91 );
 else if ( is_product_category('physical-products') )
   shop_list_page( 12 );
+else 
+  echo "asdf";
+    
 echo '</main>';
 get_footer( 'shop' );
