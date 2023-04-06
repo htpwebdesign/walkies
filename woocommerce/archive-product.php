d7615308a3af13743ab1ed8ee840c3abd01f4ca6
MiRim Yu <firepunch119@gmail.com>
2023-04-06 01:07:12 -0700

    change id for products page

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -25,7 +25,7 @@ get_header( 'shop' );
 if ( is_shop() )
   walkies_list_page( 91 );
 else if ( is_product_category('physical-products') )
-  shop_list_page( 12 );
+  shop_list_page( 537 );
 else
   category_grid_page();
     
